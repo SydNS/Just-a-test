582634679e7b15bca0e8c174c5ae13c05f1a9aaa
SydNs <sydneynsiimbe@gmail.com>
2020-11-28 07:39:24 -0800

    its 5am just refreshing my brain

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        aidah
+        val first_number = findViewById<EditText>(R.id.first_number)
+        val second_number = findViewById<EditText>(R.id.second_number)
+        val add = findViewById<Button>(R.id.add)
+        val answer = findViewById<TextView>(R.id.answer)
+        val divide = findViewById<Button>(R.id.divide)
+        val multiply = findViewById<Button>(R.id.multiply)
+        val subtract = findViewById<Button>(R.id.subtract)
+
+// the methods for calculating the various mathematical operations
+        add.setOnClickListener {
+            answer?.text = "${first_number.text.toString().toInt() + second_number.text.toString().toInt()}"
+
+        }
+        subtract.setOnClickListener {
+            answer?.text = "${first_number.text.toString().toInt() - second_number.text.toString().toInt()}"
+
+        }
+        multiply.setOnClickListener {
+            answer?.text = "${first_number.text.toString().toInt() * second_number.text.toString().toInt()}"
+
+        }
+        divide.setOnClickListener {
+            answer?.text = "${first_number.text.toString().toInt() / second_number.text.toString().toInt()}"
+
+        }
+
+
     }
 }
